ca807067976cff76f35c7e2d3c2eb45a48ec2460
Morenette <dpiere@et.esiea.fr>
2020-05-17 19:40:57 +0200

    [Feature][Archiecture]Add MVC architecture for MainScreen

--- app/src/main/java/com/example/onveutlamoyenneaumoins/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/onveutlamoyenneaumoins/presentation/view/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.example.onveutlamoyenneaumoins.Constants;
 import com.example.onveutlamoyenneaumoins.R;
 import com.example.onveutlamoyenneaumoins.data.PokeApi;
+import com.example.onveutlamoyenneaumoins.presentation.controller.MainController;
 import com.example.onveutlamoyenneaumoins.presentation.model.Pokemon;
 import com.example.onveutlamoyenneaumoins.presentation.model.RestPokemonResponse;
 import com.google.gson.Gson;
@@ -32,44 +33,28 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private SharedPreferences sharedPreferences;
-    private Gson gson;
+
+    private MainController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences =  getSharedPreferences("app_David", Context.MODE_PRIVATE);
+        controller = new MainController(
+                this,
+                new GsonBuilder()
+                        .setLenient()
+                        .create(),
+                getSharedPreferences("app_David", Context.MODE_PRIVATE)
+        );
+        controller.onStart();
 
-        gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
-        List<Pokemon> pokemonList = getDataFromCache();
-
-        if (pokemonList != null){
-            showList(pokemonList);
-        }else {
-            makeApiCall();
-        }
 
     }
 
-    private List<Pokemon> getDataFromCache() {
-
-        String jsonPokemon = sharedPreferences.getString(Constants.KEY_POKEMON_LIST, "null");
-
-        if (jsonPokemon == null){
-            return null;
-        }else {
-            Type listType = new TypeToken<List<Pokemon>>(){}.getType();
-            return gson.fromJson(jsonPokemon, listType);
-        }
-
-    }
-
-    private void showList(List<Pokemon> pokemonList) {
+    public void showList(List<Pokemon> pokemonList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
 
@@ -80,49 +65,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    static final String BASE_URL = "https://pokeapi.co/";
-
-    private void makeApiCall() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        PokeApi pokeApi = retrofit.create(PokeApi.class);
-
-        Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
-        call.enqueue(new Callback<RestPokemonResponse>() {
-            @Override
-            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
-                if (response.isSuccessful() && response.body() != null){
-                    List<Pokemon> pokemonList = response.body().getResults();
-                    showList(pokemonList);
-                    savedList(pokemonList);
-                }else {
-                    showError();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
-                showError();
-            }
-        });
-    }
-
-    private void savedList(List<Pokemon> pokemonList) {
-        String jsonString = gson.toJson(pokemonList);
-
-        sharedPreferences
-                .edit()
-                .putString("jsonPokemonList", jsonString)
-                .apply();
-
-        Toast.makeText(getApplicationContext(), "API Saved", Toast.LENGTH_SHORT).show();
-
-    }
-
-    private void showError(){
+    public void showError(){
         Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 
